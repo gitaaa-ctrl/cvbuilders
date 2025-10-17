6c69c579ae730659c2ccde569c6bf004ddc7a6d0
gitaa <adegitaq@gmail.com>
2025-10-17 21:01:19 +0700

    chore: init file

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({ children }: { children: React.ReactNode })
+{
+return (
+<html lang="en">
+<body style={{ margin: 0, fontFamily: 'system-ui, sans-serif' }}>
+<div style={{ maxWidth: 980, margin: '0 auto', padding: 16 }}>{children}
+</div>
+</body>
+</html>
+)
+}
